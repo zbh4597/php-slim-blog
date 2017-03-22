5ba274fa063d2504a01edb4230c4984bd3db4d43
zhang baohua <zhangbh33@gmail.com>
2017-03-22 20:20:34 +0800

    Create WxTokenController.php

---
+++ controller/WxTokenController.php
@@ -0,0 +1,25 @@
+<?php
+class WxTokenController{
+    protected $ci;
+    protected $logger;
+
+    public function __construct(\Slim\Container $ci) {
+       $this->ci = $ci;
+       $this->logger = $this->ci->logger;
+    }
+
+    public function blogs($request, $response, $args) {
+        $timestamp = $request->getAttribute('timestamp');
+        $nonce = $request->getAttribute('nonce');
+        $token='weixin';
+        $signature = $request->getAttribute('signature');
+        $array=array($timestamp,$nonce,$token);
+        sort($array);
+        $tmpstr=implode('', $array);
+        $tmpstr=sha1($tmpstr);
+        if($tmpstr==$signature){
+          $echostr = $request->getAttribute('echostr');
+          return $response->withJson($echostr);
+        }
+   }
+}
